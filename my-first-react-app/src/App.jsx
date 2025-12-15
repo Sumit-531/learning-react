c2242b323e438334f0c6eab8fa57b850dcfed2db
Sumit-531 <sumitchakraborty218@gmail.com>
2025-12-15 14:20:00 +0600

    add inline styles

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -5,7 +5,14 @@ import './App.css'
 
 const Card = ({title, rating}) =>{
   return(
-    <div>
+    <div style={{
+      border: '1px solid #4b5362',
+      padding: '20px',
+      margin: '10px',
+      backgroundColor: '#31363f',
+      borderRadius: '10px',
+      minHeight: '100px',
+    }}>
       <h2>{title}</h2>
       <p>Rating: {rating}</p>
     </div>
